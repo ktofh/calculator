cb030db7fd3efbfda1ba0085f5948d0ed0798a20
ktofh <iluhaodessit@gmail.com>
2022-07-10 13:59:30 +0300

    'yig'

--- calcul.js
+++ calcul.js
@@ -1,15 +1,16 @@
 const firNumber = Number(prompt('первое число')) ;
 const ope = prompt('операция (*/+-)');
 const lasNumber = Number( prompt('второе число'));
-
+parseFloat(firNumber);
+parseFloat(lasNumber);
 if (ope === '-'){
-    alert(firNumber - lasNumber)
+    alert(`Ваш результат ${firNumber - lasNumber} `)
 } else if( ope === '+'){
-    alert(firNumber + lasNumber)
+    alert(`Ваш результат ${firNumber + lasNumber} `)
 }else if(ope === '*'){
-    alert(firNumber * lasNumber)
+    alert(`Ваш результат  ${firNumber * lasNumber} `)
 }else if( ope === '/'){
-    alert(firNumber / lasNumber)
+    alert(`Ваш результат  ${firNumber / lasNumber} `)
 }else{
     alert('Такой операции не существует')
 }
